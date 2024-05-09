9e4a1fe0ed0880777810bc75c048f6ded4a47e36
obibaadoma <akrasikonadu@yahoo.com>
2024-05-09 16:05:19 +0000

    added svg clock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        body: ['"Nunito Sans"', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
